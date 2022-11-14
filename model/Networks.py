22f1e4943f8e756e79d1fec8f6c86810cf2b4da4
manhhv87 <manhhv87@gmail.com>
2022-11-14 12:15:35 +0700

    modify

--- model/Networks.py
+++ model/Networks.py
@@ -4,6 +4,10 @@ import torch.nn.functional as F
 
 
 class DoubleConv(nn.Module):
+    """
+    (convolution => [BN] => ReLU) * 2
+    """
+
     def __init__(self, in_channels, out_channels, mid_channels=None):
         super().__init__()
         if not mid_channels:
@@ -22,6 +26,10 @@ class DoubleConv(nn.Module):
 
 
 class Down(nn.Module):
+    """
+    Down-scaling with maxpool then double conv
+    """
+
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
@@ -34,9 +42,13 @@ class Down(nn.Module):
 
 
 class Up(nn.Module):
+    """
+    Up-scaling then double conv
+    """
     def __init__(self, in_channels, out_channels, bilinear=True):
         super().__init__()
 
+        # if bilinear, use the normal convolutions to reduce the number of channels
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
             self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
@@ -51,6 +63,10 @@ class Up(nn.Module):
 
         x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
                         diffY // 2, diffY - diffY // 2])
+
+        # if you have padding issues, see
+        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
+        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
 
