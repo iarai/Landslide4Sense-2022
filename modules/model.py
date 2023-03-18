8533fac15eec8e6dcaac0462fab6d683ce143d15
manhhv87 <manhhv87@gmail.com>
2023-03-18 19:01:14 +0700

    add model

---
+++ modules/model.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020-02-26 17:59
+# @Author  : Zonas
+# @Email   : zonas.wang@gmail.com
+# @File    : model.py
+"""
+"""
+import torch.nn as nn
+from collections import OrderedDict
+
+from .unet_base import *
+from .nested_unet_base import *
+from .densenet_base import *
+
+
+class UNet(nn.Module):
+    def __init__(self, cfg):
+        super(UNet, self).__init__()
+        self.n_channels = cfg.n_channels
+        self.n_classes = cfg.n_classes
+        self.bilinear = cfg.bilinear
+
+        self.inc = DoubleConv(self.n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        self.down4 = Down(512, 512)
+        self.up1 = Up(1024, 256, self.bilinear)
+        self.up2 = Up(512, 128, self.bilinear)
+        self.up3 = Up(256, 64, self.bilinear)
+        self.up4 = Up(128, 64, self.bilinear)
+        self.outc = OutConv(64, self.n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
+
+
+class NestedUNet(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+
+        self.n_channels = cfg.n_channels
+        self.n_classes = cfg.n_classes
+        self.deepsupervision = cfg.deepsupervision
+        self.bilinear = cfg.bilinear
+
+        nb_filter = [32, 64, 128, 256, 512]
+
+        self.pool = nn.MaxPool2d(2, 2)
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+        self.conv0_0 = VGGBlock(self.n_channels, nb_filter[0], nb_filter[0])
+        self.conv1_0 = VGGBlock(nb_filter[0], nb_filter[1], nb_filter[1])
+        self.conv2_0 = VGGBlock(nb_filter[1], nb_filter[2], nb_filter[2])
+        self.conv3_0 = VGGBlock(nb_filter[2], nb_filter[3], nb_filter[3])
+        self.conv4_0 = VGGBlock(nb_filter[3], nb_filter[4], nb_filter[4])
+
+        self.conv0_1 = VGGBlock(nb_filter[0]+nb_filter[1], nb_filter[0], nb_filter[0])
+        self.conv1_1 = VGGBlock(nb_filter[1]+nb_filter[2], nb_filter[1], nb_filter[1])
+        self.conv2_1 = VGGBlock(nb_filter[2]+nb_filter[3], nb_filter[2], nb_filter[2])
+        self.conv3_1 = VGGBlock(nb_filter[3]+nb_filter[4], nb_filter[3], nb_filter[3])
+
+        self.conv0_2 = VGGBlock(nb_filter[0]*2+nb_filter[1], nb_filter[0], nb_filter[0])
+        self.conv1_2 = VGGBlock(nb_filter[1]*2+nb_filter[2], nb_filter[1], nb_filter[1])
+        self.conv2_2 = VGGBlock(nb_filter[2]*2+nb_filter[3], nb_filter[2], nb_filter[2])
+
+        self.conv0_3 = VGGBlock(nb_filter[0]*3+nb_filter[1], nb_filter[0], nb_filter[0])
+        self.conv1_3 = VGGBlock(nb_filter[1]*3+nb_filter[2], nb_filter[1], nb_filter[1])
+
+        self.conv0_4 = VGGBlock(nb_filter[0]*4+nb_filter[1], nb_filter[0], nb_filter[0])
+
+        if self.deepsupervision:
+            self.final1 = nn.Conv2d(nb_filter[0], self.n_classes, kernel_size=1)
+            self.final2 = nn.Conv2d(nb_filter[0], self.n_classes, kernel_size=1)
+            self.final3 = nn.Conv2d(nb_filter[0], self.n_classes, kernel_size=1)
+            self.final4 = nn.Conv2d(nb_filter[0], self.n_classes, kernel_size=1)
+        else:
+            self.final = nn.Conv2d(nb_filter[0], self.n_classes, kernel_size=1)
+
+
+    def forward(self, input):
+        x0_0 = self.conv0_0(input)
+        x1_0 = self.conv1_0(self.pool(x0_0))
+        x0_1 = self.conv0_1(torch.cat([x0_0, self.up(x1_0)], 1))
+
+        x2_0 = self.conv2_0(self.pool(x1_0))
+        x1_1 = self.conv1_1(torch.cat([x1_0, self.up(x2_0)], 1))
+        x0_2 = self.conv0_2(torch.cat([x0_0, x0_1, self.up(x1_1)], 1))
+
+        x3_0 = self.conv3_0(self.pool(x2_0))
+        x2_1 = self.conv2_1(torch.cat([x2_0, self.up(x3_0)], 1))
+        x1_2 = self.conv1_2(torch.cat([x1_0, x1_1, self.up(x2_1)], 1))
+        x0_3 = self.conv0_3(torch.cat([x0_0, x0_1, x0_2, self.up(x1_2)], 1))
+
+        x4_0 = self.conv4_0(self.pool(x3_0))
+        x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0)], 1))
+        x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, self.up(x3_1)], 1))
+        x1_3 = self.conv1_3(torch.cat([x1_0, x1_1, x1_2, self.up(x2_2)], 1))
+        x0_4 = self.conv0_4(torch.cat([x0_0, x0_1, x0_2, x0_3, self.up(x1_3)], 1))
+
+        if self.deepsupervision:
+            output1 = self.final1(x0_1)
+            output2 = self.final2(x0_2)
+            output3 = self.final3(x0_3)
+            output4 = self.final4(x0_4)
+            return [output1, output2, output3, output4]
+
+        else:
+            output = self.final(x0_4)
+            return output
+
+
+class DenseNet(nn.Module):
+    def __init__(self, kernel_size=(3, 3, 3), n_channels=14, growth_rate=8, block_config=(4, 6, 4),
+                 num_init_features=64, bn_size=4, drop_rate=0.25,
+                 n_classes=2, memory_efficient=False):
+
+        super(DenseNet, self).__init__()
+
+        # First convolution
+        self.features = nn.Sequential(OrderedDict(
+            [('conv0', nn.Conv2d(n_channels, num_init_features, kernel_size=7, stride=2, padding=3, bias=False)),
+             ('norm0', nn.BatchNorm2d(num_init_features)),
+             ('relu0', nn.ReLU(inplace=True)),
+             ('pool0', nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+             ]))
+
+        # Each denseblock
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):  # i = [0,1,2,3] and num_layers = [6, 12, 24, 16]
+            block = _DenseBlock(kernel_size=kernel_size[i], num_layers=num_layers, num_input_features=num_features,
+                                bn_size=bn_size,
+                                growth_rate=growth_rate, drop_rate=drop_rate, memory_efficient=memory_efficient)
+            self.features.add_module('denseblock%d' % (i + 1), block)
+            num_features = num_features + num_layers * growth_rate
+            if i != len(block_config) - 1:
+                # add transition layer between denseblocks to downsample
+                trans = _Transition(num_input_features=num_features, num_output_features=num_features // 2)
+                self.features.add_module('transition%d' % (i + 1), trans)
+                num_features = num_features // 2
+
+        # Final batch norm
+        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
+
+        # Classifier layer
+        self.classifier = OutConv(num_features, n_classes)
+
+        # Official init from torch repo.
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        features = self.features(x)
+        out = F.relu(features, inplace=True)
+        out = self.classifier(out)
+        return out
