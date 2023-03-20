012470cc62fe72eb9df0ca1915d0b1ebb142b214
manhhv87 <manhhv87@gmail.com>
2023-03-20 16:37:43 +0700

    Add attention module to NestedUNet

--- modules/NestedUNet.py
+++ modules/NestedUNet.py
@@ -6,21 +6,24 @@ from modules.layers import conv_block_nested
 
 
 class ChannelAttention(nn.Module):
-    def __init__(self, in_channels, ratio = 16):
+    def __init__(self, in_channels, ratio=16):
         super(ChannelAttention, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.max_pool = nn.AdaptiveMaxPool2d(1)
-        self.fc1 = nn.Conv2d(in_channels,in_channels//ratio,1,bias=False)
+        self.fc1 = nn.Conv2d(in_channels, in_channels//ratio, 1, bias=False)
         self.relu1 = nn.ReLU()
-        self.fc2 = nn.Conv2d(in_channels//ratio, in_channels,1,bias=False)
+        self.fc2 = nn.Conv2d(in_channels//ratio, in_channels, 1, bias=False)
         self.sigmod = nn.Sigmoid()
-    def forward(self,x):
+
+    def forward(self, x):
         avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
         max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
         out = avg_out + max_out
         return self.sigmod(out)
 
 
+# https://github.com/manhhv87/Siam-NestedUNet/blob/master/models/Models.py
+# https://pub.towardsai.net/unet-clearly-explained-a-better-image-segmentation-architecture-f48661c92df9
 class NestedUNet(nn.Module):
     def __init__(self, in_ch=14, n_classes=2):
         super(NestedUNet, self).__init__()
@@ -29,7 +32,8 @@ class NestedUNet(nn.Module):
         filters = [n1, n1 * 2, n1 * 4, n1 * 8, n1 * 16]
 
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.Up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.Up = nn.Upsample(
+            scale_factor=2, mode='bilinear', align_corners=True)
 
         self.conv0_0 = conv_block_nested(in_ch, filters[0], filters[0])
         self.conv1_0 = conv_block_nested(filters[0], filters[1], filters[1])
@@ -37,25 +41,34 @@ class NestedUNet(nn.Module):
         self.conv3_0 = conv_block_nested(filters[2], filters[3], filters[3])
         self.conv4_0 = conv_block_nested(filters[3], filters[4], filters[4])
 
-        self.conv0_1 = conv_block_nested(filters[0] + filters[1], filters[0], filters[0])
-        self.conv1_1 = conv_block_nested(filters[1] + filters[2], filters[1], filters[1])
-        self.conv2_1 = conv_block_nested(filters[2] + filters[3], filters[2], filters[2])
-        self.conv3_1 = conv_block_nested(filters[3] + filters[4], filters[3], filters[3])
+        self.conv0_1 = conv_block_nested(
+            filters[0] + filters[1], filters[0], filters[0])
+        self.conv1_1 = conv_block_nested(
+            filters[1] + filters[2], filters[1], filters[1])
+        self.conv2_1 = conv_block_nested(
+            filters[2] + filters[3], filters[2], filters[2])
+        self.conv3_1 = conv_block_nested(
+            filters[3] + filters[4], filters[3], filters[3])
 
-        self.conv0_2 = conv_block_nested(filters[0]*2 + filters[1], filters[0], filters[0])
-        self.conv1_2 = conv_block_nested(filters[1]*2 + filters[2], filters[1], filters[1])
-        self.conv2_2 = conv_block_nested(filters[2]*2 + filters[3], filters[2], filters[2])
+        self.conv0_2 = conv_block_nested(
+            filters[0]*2 + filters[1], filters[0], filters[0])
+        self.conv1_2 = conv_block_nested(
+            filters[1]*2 + filters[2], filters[1], filters[1])
+        self.conv2_2 = conv_block_nested(
+            filters[2]*2 + filters[3], filters[2], filters[2])
 
-        self.conv0_3 = conv_block_nested(filters[0]*3 + filters[1], filters[0], filters[0])
-        self.conv1_3 = conv_block_nested(filters[1]*3 + filters[2], filters[1], filters[1])
+        self.conv0_3 = conv_block_nested(
+            filters[0]*3 + filters[1], filters[0], filters[0])
+        self.conv1_3 = conv_block_nested(
+            filters[1]*3 + filters[2], filters[1], filters[1])
 
-        self.conv0_4 = conv_block_nested(filters[0]*4 + filters[1], filters[0], filters[0])
+        self.conv0_4 = conv_block_nested(
+            filters[0]*4 + filters[1], filters[0], filters[0])
 
         self.ca = ChannelAttention(filters[0] * 4, ratio=16)
         self.ca1 = ChannelAttention(filters[0], ratio=16 // 4)
 
         self.conv_final = nn.Conv2d(filters[0] * 4, n_classes, kernel_size=1)
-
 
         # initialise weights
         for m in self.modules():
@@ -63,9 +76,6 @@ class NestedUNet(nn.Module):
                 init_weights(m, init_type='kaiming')
             elif isinstance(m, nn.BatchNorm2d):
                 init_weights(m, init_type='kaiming')
-
-
-        # self.final = nn.Conv2d(filters[0], n_classes, kernel_size=1)
 
     def forward(self, x):
 
@@ -86,7 +96,8 @@ class NestedUNet(nn.Module):
         x3_1 = self.conv3_1(torch.cat([x3_0, self.Up(x4_0)], 1))
         x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, self.Up(x3_1)], 1))
         x1_3 = self.conv1_3(torch.cat([x1_0, x1_1, x1_2, self.Up(x2_2)], 1))
-        x0_4 = self.conv0_4(torch.cat([x0_0, x0_1, x0_2, x0_3, self.Up(x1_3)], 1))
+        x0_4 = self.conv0_4(
+            torch.cat([x0_0, x0_1, x0_2, x0_3, self.Up(x1_3)], 1))
 
         out = torch.cat([x0_1, x0_2, x0_3, x0_4], 1)
 
@@ -95,6 +106,4 @@ class NestedUNet(nn.Module):
         out = self.ca(out) * (out + ca1.repeat(1, 4, 1, 1))
         out = self.conv_final(out)
 
-        # out = self.final(x0_4)
         return out
-        
