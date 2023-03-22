fba6069122bfa994e61cbdb0575e193a1126357c
manhhv87 <manhhv87@gmail.com>
2023-03-22 17:10:14 +0700

    Add UNet_Att to __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,3 +1,3 @@
-from .UNet import UNet
+from .UNet import UNet, UNet_Att
 from .UNet_2Plus import UNet_2Plus, UNet_2Plus_CBAM
 from .UNet_3Plus import UNet_3Plus, UNet_3Plus_CBAM, UNet_3Plus_DeepSup, UNet_3Plus_DeepSup_CGM
