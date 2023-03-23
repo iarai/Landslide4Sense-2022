689eafc06dc8958916e88ee62e997147bcc08422
manhhv87 <manhhv87@gmail.com>
2023-03-23 18:29:56 +0700

    add CBAM_ResUNet.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,3 +2,4 @@ from .UNet import UNet, UNet_Att
 from .UNet_2Plus import UNet_2Plus, UNet_2Plus_CBAM
 from .UNet_3Plus import UNet_3Plus, UNet_3Plus_CBAM, UNet_3Plus_DeepSup, UNet_3Plus_DeepSup_CGM
 from .ResUNet_2Plus import ResUNet_2Plus
+from .CBAM_ResUNet import CbamResUNet
